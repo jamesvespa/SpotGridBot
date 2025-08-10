a856a330dfe966a6da5b783a4b054605f28c5b79
james <jamesvespa01@gmail.com>
2025-08-10 08:11:43 +0100

    Added coinbase ConnectionORD

---
+++ include/coinbase/ConnectionORD.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "Definitions.h"
+//#include "ExchangeInformation.h"
+#include "RestConnectionBase.h"
+#include "JSONDocument.h"
+
+namespace CORE {
+	class ConnectionManager;
+}
+
+namespace CORE {
+namespace COINBASE {
+
+////////////////////////////////////////////////////////////////////////////
+/*! \brief OKX connection class */
+////////////////////////////////////////////////////////////////////////////
+class ConnectionORD : public CORE::RESTAPI::RestConnectionBase
+{
+public:
+	ConnectionORD(const CRYPTO::Settings &settings, const std::string &loggingPropsPath, const ConnectionManager& connectionManager);
+	
+	std::string SendOrder(const UTILS::CurrencyPair &instrument, const UTILS::Side side, const RESTAPI::EOrderType orderType,
+						  const UTILS::TimeInForce timeInForce, const double price, const double quantity,
+						  const std::string &clientOrderId = "");
+	
+	std::string QueryOrder(const UTILS::CurrencyPair &instrument, const std::string &orderId,
+						   const std::optional<std::string> &origClientOrderId = std::nullopt);
+	
+	std::string CancelOrder(const UTILS::CurrencyPair &instrument, const std::string &orderId,
+							const std::optional<std::string> &origClientOrderId = std::nullopt);
+
+	TExecutionReports TranslateOrderResult(const std::shared_ptr<CRYPTO::JSONDocument> jd) const override;
+	
+	TExecutionReports TranslateOrder(const std::shared_ptr<CRYPTO::JSONDocument> jd, Poco::Logger *logger = nullptr) const override;
+
+	static std::tuple<char, char> TranslateOrderStatus(const std::string &status);
+
+protected:
+
+	// Web request wrapper
+	std::string DoWebRequest(const std::string &url, const std::string &requestType,
+							 std::function<void(std::string &path)> customizeRequestPathFunc = nullptr,
+							 std::function<void(Poco::Net::HTTPRequest &request)> customizeRequestFunc = nullptr);
+
+	virtual void OnMsgResult(const std::string &result, const int id, const UTILS::BoolResult &res);
+	virtual void OnMsgError(const int errCode, const std::string &errMsg, const UTILS::BoolResult &res);
+	
+private:
+
+	enum class ERequestType
+	{
+		QueryOrder, CancelOrder
+	};
+	
+	std::string QueryOrderOrCancel(const ERequestType requestType, const UTILS::CurrencyPair &instrument, const std::string &orderId,
+								   const std::optional<std::string> &origClientOrderId = std::nullopt);
+
+};
+
+} // ns BINANCE
+}
